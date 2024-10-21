3d77c897a87e1ae1535eb284905e216c5e53e33d
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-10-21 15:03:14 +0100

    use refactored custom button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, Pressable } from 'react-native';
 import { useRouter } from 'expo-router';
+import CustomButton from '@/components/CustomButton';
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -9,28 +10,14 @@ export default function HomeScreen() {
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Hello climber!</Text>
       </View>
-      <Pressable
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? '#286796' : '#4886b5',
-          },
-          styles.button,
-        ]}
+      <CustomButton
+        title="Log a Climb"
         onPress={() => router.push('/(tabs)/LogAClimb')}
-      >
-        <Text style={styles.buttonText}>Log a Climb</Text>
-      </Pressable>
-      <Pressable
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? '#286796' : '#4886b5',
-          },
-          styles.button,
-        ]}
-        onPress={() => router.push('/(tabs)/LogAClimb')}
-      >
-        <Text style={styles.buttonText}>See logged climbs</Text>
-      </Pressable>
+      />
+      <CustomButton
+        title="See logged climbs"
+        onPress={() => router.push('/(tabs)/LoggedClimbs')}
+      />
     </>
   );
 }
@@ -43,20 +30,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  button: {
-    paddingVertical: 15,
-    paddingHorizontal: 25,
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: 20,
-    width: '80%',
-    alignSelf: 'center',
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 18,
-    fontWeight: 'bold',
   },
 });
